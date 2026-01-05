a0029673c612d8039eb55210dca291952f56e723
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2026-01-05 05:57:22 +0530

    feat: 3.2. mid

---
+++ 3_2_Data_Types.rs
@@ -0,0 +1,26 @@
+fn main(){
+    // Data Types
+    // Scaler datatypes represent single value
+    // Compound datatypes represent group of values
+
+    // Scaler datatypes
+    // Integers
+    // Floating-point numbers
+    // Booleans
+    // Character
+
+    // Integers
+    // Signed:      either +ve/-ve integers
+    // unsigned:    only +ve integers
+    
+    // Integer Types in Rust
+    // Length	Signed	Unsigned
+    // 8-bit	i8	u8
+    // 16-bit	i16	u16
+    // 32-bit	i32	u32
+    // 64-bit	i64	u64
+    // 128-bit	i128	u128
+    // Architecture-dependent	isize	usize
+
+    // Architecture-dependent: the isize and usize types depend on the architecture of the computer your program is running on: 64 bits if you’re on a 64-bit architecture and 32 bits if you’re on a 32-bit architecture.
+}
